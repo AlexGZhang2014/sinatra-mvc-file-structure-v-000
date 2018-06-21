720e7b57c3f9f17da90fbe64a93d1645d5559cc2
Alex Zhang <alexgzhang92@gmail.com>
2018-06-21 01:45:59 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,10 +1,17 @@
 class Dog
   attr_accessor :name, :breed, :age
   
+  @@all = []
+  
+  def self.all
+    @@all
+  end
+  
   def initialize(name, breed, age)
     @name = name
     @breed = breed
     @age = age
+    self.all << self
   end
   
 end
